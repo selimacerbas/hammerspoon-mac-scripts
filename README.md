e39049015e8a7a7a51aa98b39bfeb97ca8554c03
taaacse4 <selim.acerbas@swisscom.com>
2025-09-06 11:55:57 +0200

    add PaperWM and SpoonInstaller

---
+++ README.md
@@ -0,0 +1,2 @@
+#Example: Manual Installation
+git clone https://github.com/mogenson/PaperWM.spoon ~/.hammerspoon/Spoons/PaperWM.spoon
--- init.lua
+++ init.lua
@@ -3,8 +3,20 @@ hs.loadSpoon("Vifari")
 hs.loadSpoon("Displays")
 hs.loadSpoon("MissionControl")
 hs.loadSpoon("KeyCaster")
+hs.loadSpoon("SpoonInstall")
 
+spoon.SpoonInstall.repos.PaperWM = {
+    url = "https://github.com/mogenson/PaperWM.spoon",
+    desc = "PaperWM.spoon repository",
+    branch = "release",
+}
 
+spoon.SpoonInstall:andUse("PaperWM", {
+    repo = "PaperWM",
+    config = { screen_margin = 16, window_gap = 2 },
+    start = true,
+    hotkeys = PaperWM and PaperWM.default_hotkeys or nil, -- use defaults
+})
 
 -- Optional: For column
 spoon.KeyCaster:configure({
